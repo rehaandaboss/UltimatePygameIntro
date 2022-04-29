d46a4705c47ea83512215710f02a774409ec9054
rehaandaboss <rehaanchowdhary2021@gmail.com>
2022-04-29 11:14:22 +0400

    Update runner_video.py

--- runner_video.py
+++ runner_video.py
@@ -123,7 +123,6 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 game_active = False
 start_time = 0
 score = 0
